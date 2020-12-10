f3a6f4ae417d740469e274440e27ccf315569b14
kabirrrr19 <63115195+kabirrrr19@users.noreply.github.com>
2020-12-10 16:12:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+@st.cache(allow_output_mutation = True)
+def load_model():
+    model = tf.keras.models.load_model(r"C:\Users\Atul\Downloads\stkabir\basic_cnn.h5")
+    return model
+
+model = load_model()
+
+html_temp = """
+    <div style="background-color:black;"><p style="color:white;font-size:40px;padding:9px">Malaria Detection Using Deep Learning</p></div>
+    
+    """
+st.markdown(html_temp, unsafe_allow_html=True)
+
+st.sidebar.info("Artificial intelligence (AI) and open source tools, technologies, and frameworks are a powerful combination for improving society. 'Health is wealth' is perhaps a cliche, yet it's very accurate! We will use how AI can be leveraged for detecting the deadly disease malaria with a low-cost, effective, and accurate yet open source deep learning solution.")
+# st.sidebar.text("Credits to Kabir")
+    
+st.sidebar.header("For Any Queries/Suggestions Please reach out at :")
+st.sidebar.info("kabirrrr0402@gmail.com")
+
+file = st.file_uploader('Upload the Cell Image', type=['jpg','png'])
+# import cv2
+from PIL import Image, ImageOps
+def import_and_predict(image_data, model):
+    size = (125, 125)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
+
+    return prediction
+
+if file is None:
+    st.text("Upload the Cell Image here")
+else:
+    image  = Image.open(file)
+    st.image(image, use_column_width = True)
+    predictions = import_and_predict(image, model)
+    if predictions > 0.5:
+	    string = "You have Malaria."
+    else:
+	    string = "You Don't have Malaria."
+    # class_names = ["have Malaria", "don't have Malaria"]
+    # string = "Your report after analyzing your Cell image is you " + class_names[np.argmax(predictions)]
+    st.success(string)
+    
+
+if st.button("Exit"):
+        st.balloons()
